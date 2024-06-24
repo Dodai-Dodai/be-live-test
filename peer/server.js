d9e152d17e3263c7c85b638754e4ac31b90452d7
Dodai-Dodai <59632014nen@gmail.com>
2024-06-24 22:11:57 +0900

    peerの作成時にhttpsをon

---
+++ peer/server.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const https = require('https');
+const { PeerServer } = require('peer');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/be-live.ytakag.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/be-live.ytakag.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/be-live.ytakag.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+const httpsServer = https.createServer(credentials);
+
+const peerServer = PeerServer({
+  port: 9000,
+  path: '/peerjs',
+  ssl: credentials
+});
+
+peerServer.on('connection', (client) => {
+  console.log(`Client connected: ${client.getId()}`);
+});
+
+peerServer.on('disconnect', (client) => {
+  console.log(`Client disconnected: ${client.getId()}`);
+});
+
+httpsServer.listen(9000, () => {
+  console.log('PeerJS server running on https://be-live.ytakag.com:9000');
+});
